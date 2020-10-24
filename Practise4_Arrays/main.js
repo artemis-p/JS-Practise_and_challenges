37628b1b822410f2c5aa1c77315dcdf75e2ec94c
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-10-24 21:57:01 +0100

    Arrays and ice cream flavours challenge. Half of bubblegum challenge

---
+++ Practise4_Arrays/main.js
@@ -0,0 +1,42 @@
+// var flavours = ["vanilla", "butterscotch", "lavender", "chocolate", "cookie dough"];
+// var flavourOfTheDay = flavours[3];
+// console.log(flavourOfTheDay);
+
+// flavours[3] = "strawberry";
+// console.log(flavours);
+
+// var numFlavours = flavours.length;
+// console.log(numFlavours);
+
+// var last = flavours.length - 1;
+// var recent = flavours[last];
+// console.log(recent);
+
+// var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69];
+
+// function bubbleSolution() {
+//     for (var i = 0; i < scores.length; i++) {
+//         console.log("Bubble solution #" + [i] + " score: " + scores[i]);
+//     }
+// }
+
+// bubbleSolution();
+
+// var highestScore = Math.max.apply(0, scores);
+
+//     console.log("Bubble tests: " + scores.length);  
+//     console.log("Highest bubble score: " + highestScore);
+//     console.log("Solutions with highest score:")
+
+
+// var products = ["Icy Mint", "Bubblegum"];
+// var hasBubbleGum = [false, true];
+// var i = 0;
+
+// while (i < hasBubbleGum.length) {
+//     if (hasBubbleGum[i]){
+//         console.log(products[i] + " contains bubblegum");
+//     }
+//     i += 1;
+// }
+
