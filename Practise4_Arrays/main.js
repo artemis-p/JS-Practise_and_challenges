16d467f6eec6e38e36d04e7289416802513ccbc6
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-10-26 16:34:25 +0000

    Refactoring of the bubble quiz solution

--- Practise4_Arrays/main.js
+++ Practise4_Arrays/main.js
@@ -12,32 +12,6 @@
 // var recent = flavours[last];
 // console.log(recent);
 
-var scores = [60, 50, 69, 58, 54, 54, 58, 50, 52, 54, 48, 69];
-var highScore = 0;
-var output;
-var bestSolutions = [];
-
-    for (var i = 0; i < scores.length; i++) {
-        output = ("Bubble solution #" + i + " score: " + scores[i]);
-        console.log(output);
-        if (scores[i] > highScore) {
-            highScore = scores[i];
-        }
-    }
-
-    console.log("Bubble tests: " + scores.length);  
-    console.log("Highest bubble score: " + highScore);
-
-    for (var i = 0; i < scores.length; i++) {
-        if (scores[i] == highScore) {
-            bestSolutions.push("#" + [i]);
-        }
-    }
-    console.log("Solutions with highest score: " + bestSolutions);
-
-
-
-
 
 // var products = ["Icy Mint", "Bubblegum"];
 // var hasBubbleGum = [false, true];
@@ -61,3 +35,38 @@ var bestSolutions = [];
 // // or add them by using the push method
 // genres.push("folk");
 // console.log(genres);
+
+
+var scores = [60, 50, 69, 58, 54, 54, 58, 50, 52, 54, 48, 69];
+
+
+function printAndGetHighScore() {
+    var highScore = 0;
+    var output;
+    for (var i = 0; i < scores.length; i++) {
+        output = ("Bubble solution #" + i + " score: " + scores[i]);
+        console.log(output);
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+
+function getBestResults(scores, highScore) { 
+    var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            bestSolutions.push("#" + [i]);
+        }
+    }
+    return bestSolutions;
+}
+
+
+var highScore = printAndGetHighScore(scores);
+console.log("Bubble tests: " + scores.length);  
+console.log("Highest bubble score: " + highScore);
+
+var bestSolutions = getBestResults(scores, highScore);
+console.log("Solutions with highest score: " + bestSolutions);
