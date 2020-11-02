02260a935c8fdc89b10e4a24ffa5a8f52b61c6bd
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-11-02 21:16:12 +0000

    Methods on string objects

---
+++ Practise7_Types,Equality,etc/main.js
@@ -0,0 +1,6 @@
+var text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
+var presentableText = text.toLowerCase();
+if (presentableText.length > 0) {
+    alert(presentableText);
+}
+console.log(presentableText);
