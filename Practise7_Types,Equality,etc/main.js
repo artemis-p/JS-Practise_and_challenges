6c8e6e4d0a2f12d8cf4dba1c35196bbf838c730d
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-11-10 17:15:23 +0000

    Using instanceOf

--- Practise7_Types,Equality,etc/main.js
+++ Practise7_Types,Equality,etc/main.js
@@ -1,11 +1,23 @@
-var text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
-var presentableText = text.toLowerCase();
-if (presentableText.length > 0) {
-    alert(presentableText);
-}
-console.log(presentableText);
+// var text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
+// var presentableText = text.toLowerCase();
+// if (presentableText.length > 0) {
+//     alert(presentableText);
+// }
+// console.log(presentableText);
 
-var phoneNumber = "123-4567";
-function validate(phoneNumber) {
-    return phoneNumber.match(/^\d{3}-?\d{4}$/);
+// var phoneNumber = "123-4567";
+// function validate(phoneNumber) {
+//     return phoneNumber.match(/^\d{3}-?\d{4}$/);
+// }
+
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function() { console.log(this.sound); }
 }
+
+var toy = new Duck("quack quack");
+
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck); 
