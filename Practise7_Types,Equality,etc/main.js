adc2c44c1d2a7019f469258edd1bc72a7bbc46ef
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-11-10 15:58:18 +0000

    Validating phone number with regex

--- Practise7_Types,Equality,etc/main.js
+++ Practise7_Types,Equality,etc/main.js
@@ -4,3 +4,8 @@ if (presentableText.length > 0) {
     alert(presentableText);
 }
 console.log(presentableText);
+
+var phoneNumber = "123-4567";
+function validate(phoneNumber) {
+    return phoneNumber.match(/^\d{3}-?\d{4}$/);
+}
